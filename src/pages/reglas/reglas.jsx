0a5f588c071c2389ba316dfc14de349e25be8b0b
mdemartino26 <m.demartino99@gmail.com>
2024-04-17 19:43:34 -0300

    pagina reglas

--- src/pages/reglas/reglas.jsx
+++ src/pages/reglas/reglas.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header2 from '../../components/header2/header2';
 
 function Reglas() {
     
 
     return (
+
         <div>
+            <Header2 />
             <h1>Reglas</h1>
+            <ul>
+                <li>Leer bien las consignas</li>
+                <li>Si se traban, pueden consutlar</li>
+                <li>Tienen que observar bien</li>
+                <li>¡Diviertanse!</li>
+            </ul>
             <Link to="/numero1"><button>Siguiente</button></Link>
         </div>
     );
