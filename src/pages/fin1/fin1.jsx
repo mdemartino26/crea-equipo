3fefb73536f9b091f585f0b7a0c807599e477ee1
mdemartino26 <m.demartino99@gmail.com>
2024-04-18 00:01:14 -0300

    detalles finales de gifs

--- src/pages/fin1/fin1.jsx
+++ src/pages/fin1/fin1.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import Header2 from '../../components/header2/header2';
+import Decor2 from '../../components/decor2/decor2';
+import './fin1.css';
+import Sobre from '../../assets/img/congratulations-envelope.gif';
 
 function Fin1() {
     
     return (
-        <div>
-            <h2>Felicitaciones! han ganado el juego </h2>
-            <p> ¡Podes ir a buscar tu sobre! </p>
-        </div>
+        <>
+        <div className='findj'>
+            <Header2/>
+            <div className="main">
+            <h2>¡Felicitaciones! han ganado el juego </h2>
+            <p> ¡Pueden ir a buscar tu sobre! </p>
+            <br />
+            <br />
+            <img src={Sobre} alt="Felicitaciones" />
+            <p>Gracias por participar</p>
+            </div>
+            </div>
+            <Decor2/>
+       
+        </>
     );
 }
 
