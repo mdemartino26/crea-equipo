705e6a6feea150c52884b2f30a6d80954add4bef
mdemartino26 <m.demartino99@gmail.com>
2024-04-27 15:56:11 -0300

    sonidos de correcto e incorrect en consigna 1

--- src/pages/numero1/numero1.jsx
+++ src/pages/numero1/numero1.jsx
@@ -1,14 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import Header2 from '../../components/header2/header2';
 import './numero1.css';
 import Decor from '../../components/decor/decor';
+import CorrectSound from '../../assets/sounds/correct.mp3';
+import WrongSound from '../../assets/sounds/wrong.mp3';
 
 function Numero1() {
     const [respuesta, setRespuesta] = useState('');
     const [error, setError] = useState(false);
     const [respuestaCorrecta, setRespuestaCorrecta] = useState(false);
     const [mostrarSiguiente, setMostrarSiguiente] = useState(true);
+    const correctSoundRef = useRef(null);
+    const wrongSoundRef = useRef(null);
 
     const handleChange = (event) => {
         setRespuesta(event.target.value);
@@ -23,38 +27,43 @@ function Numero1() {
             console.log('Respuesta correcta');
             setRespuestaCorrecta(true);
             setMostrarSiguiente(false);
+            correctSoundRef.current.play(); // Play correct sound
         } else {
             // Incorrect answer, show error
             setError(true);
+            wrongSoundRef.current.play(); // Play wrong sound
         }
     };
 
     return (
-        <div >
+        <div>
             <Header2 />
             <div className='main'>
-            <p>Ellas son blancas. Intentan con todas sus fuerzas unirse, pero no lo logran. (número)</p>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={respuesta}
-                    onChange={handleChange}
-                    placeholder="Ingrese su respuesta"
-                />
-                <br />
-                {mostrarSiguiente && <button type="submit" class="buttonPpal" >Siguiente</button>}
-          
-            {error && <p style={{ color: 'red' }}>Respuesta incorrecta. Intente de nuevo.</p>}
-            {respuestaCorrecta && (
-                <>
-                    <p style={{ color: 'green' }}>¡Correcto!</p>
-                    <Link to="/numero2">
-                        <button class="buttonPpal" >Continuar</button>
-                    </Link>
-                </>
-            )}</form>
+                <p>Ellas son blancas. Intentan con todas sus fuerzas unirse, pero no lo logran. (número)</p>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        value={respuesta}
+                        onChange={handleChange}
+                        placeholder="Ingrese su respuesta"
+                    />
+                    <br />
+                    {mostrarSiguiente && <button type="submit" className="buttonPpal">Siguiente</button>}
+
+                    {error && <p style={{ color: 'red' }}>Respuesta incorrecta. Intente de nuevo.</p>}
+                    {respuestaCorrecta && (
+                        <>
+                            <p style={{ color: 'green' }}>¡Correcto!</p>
+                            <Link to="/numero2">
+                                <button className="buttonPpal">Continuar</button>
+                            </Link>
+                        </>
+                    )}
+                </form>
             </div>
-            <Decor/>
+            <Decor />
+            <audio ref={correctSoundRef} src={CorrectSound}></audio>
+            <audio ref={wrongSoundRef} src={WrongSound}></audio>
         </div>
     );
 }
